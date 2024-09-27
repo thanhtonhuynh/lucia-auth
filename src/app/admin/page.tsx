581794bf2f933008e2efd27a9c4cf02f582fcbb9
Ton Huynh <kintonhtt@gmail.com>
2024-09-26 22:38:35 -0700

    add admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,25 @@
+import { validateRequest } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Page() {
+  const { user } = await validateRequest();
+
+  if (!user) {
+    redirect('/login');
+  }
+
+  if (user.role !== 'admin') {
+    return (
+      <main className="mx-auto my-10">
+        <p className="text-center">You are not authorized to view this page.</p>
+      </main>
+    );
+  }
+
+  return (
+    <main className="mx-auto my-10 space-y-3">
+      <h1 className="text-center text-xl font-bold">Admin Page</h1>
+      <p className="text-center">Welcome, {user.name}!</p>
+    </main>
+  );
+}
